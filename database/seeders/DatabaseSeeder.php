e7864175dec750374537e051d99b7696f48ceab8
morozov <morozovprav@gmail.com>
2022-10-27 22:55:25 +0100

    update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Cache;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        if ($this->command->confirm('Do you want to refresh the database?')){
+            $this->command->call('migrate:refresh');
+            $this->command->info('Database was refreshed!');
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UsersTableSeeder::class,
+            CategoryTableSeeder::class,
+            PostsTableSeeder::class,
+        ]);
     }
 }
