0885a8c49c3feb1ae5df16a40a0762c0b42af9d7
morozov <morozovprav@gmail.com>
2022-10-27 23:16:31 +0100

    add category controller and request

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\CategoryRequest;
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class CategoryController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        if (count($categories) < 1){
+            $categories = 'No categories available';
+        }
+        return $this->sendResponse($categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(CategoryRequest $request)
+    {
+        $validated = $request->validated();
+        $category = new Category();
+        $category = $category->create($validated);
+
+        return $this->sendResponse($category);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Category $category)
+    {
+        return $this->sendResponse($category);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(CategoryRequest $request, $id)
+    {
+        $category = Category::findOrFail($id);
+        $validated = $request->validated();
+        $category->fill($validated);
+        $category->save();
+        return $this->sendResponse($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return $this->sendResponse('post was deleted');
+    }
+}
