299d9bde0d8005476d244952c3d1c9f874dacec0
morozov <morozovprav@gmail.com>
2022-10-27 22:55:02 +0100

    add post and users seeder

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $blogCount = (int)$this->command->ask('How many blog posts would you like?', 50);
+        $categories = Category::query()->where('status', true)->get();
+        Post::factory($blogCount)->create()->each(function ($post) use($categories){
+            $post->categories()->attach($categories->random()->id);
+            $post->save();
+        });
+    }
+}
