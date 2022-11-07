034004fbe1516d3a273a77ee0609ab9ab33e9a0d
morozov <morozovprav@gmail.com>
2022-11-07 04:38:53 +0000

    add categories link to site menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <h5 class="my-0 me-md-auto font-weight-normal">Blog Ascensor</h5>
     <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark" href="{{ route('posts.index') }}">Posts</a>
-        <a class="p-2 text-dark disabled" href="{{ route('posts.index') }}">Category</a>
+        <a class="p-2 text-dark disabled" href="{{ route('categories.index') }}">Category</a>
         <a class="p-2 text-dark disabled" href="{{ route('posts.index') }}">Users</a>
 
         @guest
